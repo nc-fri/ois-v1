0dd5f915b3112e54aa18fb80ac2f97a986a4894a
Nik Cof <nik.cof123@gmail.com>
2023-02-21 21:34:47 +0100

    Dodana slika in izpis pospeška ter nadmorske višine

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,7 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon");
     }
+    public static void izpis(int visina, double gravitacija) {
+        System.out.printf("Gravitacijski pospešek znaša %f na nadmorski višini %d",gravitacija, visina);
+    }
 }
