15dad92ddf41c9de4e64004c4c12dfe32cb0db1f
K-Kuhar <kk47158@student.uni-lj.si>
2023-02-21 21:29:28 +0100

    poskus za commit #2, spremenjena Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,15 @@
 public class Gravitacija {
+
+    private static final double C = 6.674 * Math.pow(10,-11);
+    private static final double M = 5.972 * Math.pow(10,24);
+    private static final double r = 6.371 * Math.pow(10,6);
+
+
     public static void main(String[] args) {
         System.out.println("OIS je zakon");
+    }
+
+    public static double gravitacija(int visina){
+        return (C * M) / Math.pow((r + visina),2);
     }
 }
